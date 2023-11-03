8de4d30e5f7cd5be51a13e12207d1640ec448e6a
Ryan4253 <ryan.liao0305@gmail.com>
2023-11-03 11:59:14 -0400

    Add turning toward target

--- Main.py
+++ Main.py
@@ -1,28 +1,30 @@
 import cv2
 from detection import detect_people
 import time
-
+from PID import PID 
+from Serial import setTurretPower
 
 def initialize():
     cap.set(3, 1280) # WIDTH
     cap.set(4, 720) # HEIGHT
     time.sleep(5)
 
-print("WASSUP")
-cap = cv2.VideoCapture(1
-                       )
+cap = cv2.VideoCapture(1)
+turnPID = PID(100, 0, 0)
+turnPID.setTarget(0.5)
 initialize()
+
 while True:
-    print("MOREHI")
     time.sleep(0.33)
     _, frame = cap.read()
-    cv2.imshow("PIC", frame)
+    #cv2.imshow("PIC", frame)
     result = detect_people(frame)
 
     if result is None:
         print("NOPE")
         continue
 
-    print(result)
+    power = turnPID.step(result[0], 0.33)
+    setTurretPower(power)
 
 
